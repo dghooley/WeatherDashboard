2514e65e6351012771f957ebbc853fe6bda8823b
Derick Hooley <dghooley@gmail.com>
2021-09-18 11:51:26 -0500

    humidity, wind

--- assets/weather.js
+++ assets/weather.js
@@ -1,63 +1,66 @@
 $(document).ready(function(){
+// APIkey 
 var APIkey = "401527215f995265731bc0cc2e824f7a"
 
-
-
+// search button activation
 $("#searchButton").on("click", function(){
 var userSearch = $("#searchBar").val()
-console.log(userSearch)
+// console.log(userSearch)
 
+// function using api to retrieve current weather data
 getCurrentWeather(userSearch)
-
 })
-
 function getCurrentWeather(userSearch){
-    console.log("user search in getCurrentWeather", userSearch)
+  console.log("user search in getCurrentWeather", userSearch)
 var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${APIkey}&units=imperial`
-console.log("currentWEatherURL", currentWeatherURL)
+  console.log("currentWEatherURL", currentWeatherURL)
 
+// updating webpage with current weather without a page reload
 $.ajax({
     url: currentWeatherURL,
     method: "GET"
 }).then(function(response){
-    console.log(response);
+  console.log(response);
 
+// creating variables based on location in DOM
 var cityName = response.name;
-
+  console.log()
 var temperature = response.main.temp;
-//console.log()
-
+  console.log()
 var humidity = response.main.humidity;
-//console.log()
-
+  console.log()
 var wind = response.wind.speed;
+  console.log()
 
+// creating dynamic HTML content to display city, temp, humidity, wind and UV index data
 var cityNameEl = $("<h2 class='card-title text-center'>").text(cityName);
 var tempElement = $("<p>").addClass("card-text").text("Temperature: " + temperature + "F")
+var humidityElement = $("<p>").addClass("card-text").text("Humidity: " + humidity + "")
+var windElement = $("<p>").addClass("card-text").text("Wind: " + wind + "mph")
 
 $("#currentWeather").append(cityNameEl, tempElement)
+$("#currentWeather").append(cityNameEl, humidityElement)
+$("#currentWeather").append(cityNameEl, windElement)
 
+// creating variables based on location in DOM
 var coords = {
     lat: response.coord.lat,
     lon: response.coord.lon
 }
 
 getFiveDay(coords)
-
-
 })
-
 }
-
+// function using API to retrieve five day weather forecast
 function getFiveDay(coords){
-    console.log("coords in five day", coords)
+// console.log("coords in five day", coords)
     var fiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`
-
+// updating webpage with coordinates without a reload
     $.ajax({
         url: fiveDayURL,
         method: "GET"
     }).then(function(response){
-        console.log("FIVE DAY RESPONSE",response);
+// console.log("FIVE DAY RESPONSE",response);
 }
     )}
 
