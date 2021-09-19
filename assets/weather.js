efd128dc292f72b36406de5c4148b97a488b61c8
Derick Hooley <dghooley@gmail.com>
2021-09-19 16:03:13 -0500

    lat/lon

--- assets/weather.js
+++ assets/weather.js
@@ -2,16 +2,7 @@ $(document).ready(function(){
 // APIkey 
 var APIkey = "401527215f995265731bc0cc2e824f7a"
 
-let date = new Date();
-
-// search button activation
-/*$("#userSearch").keypress(function(event){
-  if (event.keyCode === 13)  {
-    event.preventDefault();
-    $("#searchButton").click();
-  }
-});
-*/
+// let date = new Date();
 
 $("#searchButton").on("click", function(){
 var userSearch = $("#searchBar").val()
@@ -21,18 +12,16 @@ var userSearch = $("#searchBar").val()
 getCurrentWeather(userSearch)
 })
 function getCurrentWeather(userSearch){
-  console.log("user search in getCurrentWeather", userSearch)
+//  console.log("user search in getCurrentWeather", userSearch)
 var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${APIkey}&units=imperial`
-  console.log("currentWEatherURL", currentWeatherURL)
+//  console.log("currentWEatherURL", currentWeatherURL)
 
-// updating webpage with current weather without a page reload
 $.ajax({
     url: currentWeatherURL,
     method: "GET"
 }).then(function(response){
-  console.log(response);
+//  console.log(response);
 
-// creating variables based on location in DOM
 var cityName = response.name;
   console.log()
 var temperature = response.main.temp;
@@ -41,46 +30,37 @@ var humidity = response.main.humidity;
   console.log()
 var wind = response.wind.speed;
   console.log()
-
+// creating variables based on location in DOM
+var coords = {
+  lat: response.coord.lat,
+  lon: response.coord.lon
+}  
 // creating dynamic HTML content to display city, temp, humidity, wind and UV index data
-var cityNameEl = $("<h1 class='card-title align-left'>").text(cityName);
+var cityNameEl = $("<h2> class='card-title align-left'>").text(cityName);
 var tempElement = $("<p>").addClass("card-text").text("Temperature: " + temperature + "F")
 var humidityElement = $("<p>").addClass("card-text").text("Humidity: " + humidity + "%")
 var windElement = $("<p>").addClass("card-text").text("Wind: " + wind + " MPH")
+var coordsElement = $("<p>").addClass("card-text").text("Lat: " + coords.lat + ", Lon: " + coords.lon)
+$("#currentWeather").append(cityNameEl)
+$("#currentWeather").append(tempElement)
+$("#currentWeather").append(humidityElement)
+$("#currentWeather").append(windElement)
+$("#currentWeather").append(coordsElement)
 
-$("#currentWeather").append(cityNameEl, tempElement)
-$("#currentWeather").append(cityNameEl, humidityElement)
-$("#currentWeather").append(cityNameEl, windElement)
-
-// creating variables based on location in DOM
-var coords = {
-    lat: response.coord.lat,
-    lon: response.coord.lon
-}
-
-const currentUVEl= document.getElementById("UV-index");
-let =UVqURL="https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-axios.get(UVqURL)
-.then(function(response) {
-  let UVIndex=document.createElement("span");
-  UVIndex.setAttribute("class", "badge badge-danger");
-  currentUVEl.innerHTML="UV Index: ";
-  currentUVEl.append(UVIndex);
-})
 
 getFiveDay(coords)
 })
 }
 // function using API to retrieve five day weather forecast
 function getFiveDay(coords){
-// console.log("coords in five day", coords)
+  console.log("coords in five day", coords)
     var fiveDayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${APIkey}`
 // updating webpage with coordinates without a reload
     $.ajax({
         url: fiveDayURL,
         method: "GET"
     }).then(function(response){
-// console.log("FIVE DAY RESPONSE",response);
+  console.log("FIVE DAY RESPONSE",response);
 }
     )}  
 })
